4f1ed5ce0f3f4b70b7f9ebc5a79973f0a23cef9b
Jason Hite <hite77@gmail.com>
2025-02-22 05:30:05 +0000

    Day 5 part 2 done!

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -75,6 +75,59 @@ defmodule Day5 do
       [x  ,  y] when x > y -> false
     end
   end
+  @doc """
+
+  ## Examples
+      iex> orders =   [[47,53],
+      ...>            [97,13],
+      ...>            [97,61],
+      ...>            [97,47],
+      ...>            [75,29],
+      ...>            [61,13],
+      ...>            [75,53],
+      ...>            [29,13],
+      ...>            [97,29],
+      ...>            [53,29],
+      ...>            [61,53],
+      ...>            [97,53],
+      ...>            [61,29],
+      ...>            [47,13],
+      ...>            [75,47],
+      ...>            [97,75],
+      ...>            [47,61],
+      ...>            [75,61],
+      ...>            [47,29],
+      ...>            [75,13],
+      ...>            [53,13]]
+      iex> Day5.sortInvalid([61,13,29], orders, orders)
+      [61,29,13]
+      iex> Day5.sortInvalid([75,97,47,61,53], orders, orders)
+      [97,75,47,61,53]
+      iex> Day5.sortInvalid([97,13,75,29,47], orders, orders)
+      [97,75,47,29,13]
+  """
+  def sortInvalid(updates, [], _orders), do: updates
+  def sortInvalid(updates, [head | tail], orders) do
+    [first,second] = head
+    firstIndex = Enum.find_index(updates, fn item -> item == first end)
+    secondIndex = Enum.find_index(updates, fn item -> item == second end)
+    case [firstIndex, secondIndex] do
+      [nil, _y] -> sortInvalid(updates, tail, orders)
+      [x  ,  y] when x < y -> sortInvalid(updates, tail, orders)
+      [x  ,  y] when x > y -> sortInvalid(updates |> Enum.map(fn update -> swap(update,firstIndex,secondIndex, updates) end), orders, orders)
+    end
+
+  end
+  # update
+  #    p1 p2
+  # 61 13 29
+  def swap(update, pos1, pos2, updates) do
+    case [update, Enum.at(updates, pos1), Enum.at(updates, pos2)] do
+      [update, update, _] -> Enum.at(updates, pos2)
+      [update, _, update] -> Enum.at(updates, pos1)
+      [_, _, _]           -> update
+    end
+  end
 
   def convert([one, two]) do
     [String.to_integer(one), String.to_integer(two)]
@@ -91,6 +144,24 @@ defmodule Day5 do
     end
   end
 
+  def addupCorrectedMiddleNumbers(update, orders) do
+    case validUpdate(orders, update) do
+      false -> update |> sortInvalid(orders, orders) |> middle()
+      true -> 0
+    end
+  end
+
+  def commonSetup(contents) do
+    [raworders, rawupdates] = contents |> String.split("\n\n", trim: true)
+    orders = raworders |> String.split("\n")
+                       |> Enum.map(fn order -> order |> String.split("|") end)
+                       |> Enum.map(fn order -> convert(order) end )
+    updates = rawupdates |> String.split("\n")
+                         |> Enum.map(fn update -> update |> String.split(",") end)
+                         |> Enum.map(fn item -> convertTuple(item) end)
+    [orders, updates]
+  end
+
    @doc """
   Solve first the sample, then the days exercise.
 
@@ -99,14 +170,22 @@ defmodule Day5 do
       143
   """
   def part1(contents) do
-    [raworders, rawupdates] = String.split(contents, "\n\n", trim: true)
-    orders = raworders |> String.split("\n")
-                       |> Enum.map(fn order -> order |> String.split("|") end)
-                       |> Enum.map(fn order -> convert(order) end )
-    rawupdates |> String.split("\n")
-               |> Enum.map(fn update -> update |> String.split(",") end)
-               |> Enum.map(fn item -> convertTuple(item) end)
-               |> Enum.reduce(0, fn update, acc -> acc + addUpValidMiddleNumbers(update, orders) end)
+    [orders, updates] = contents |> commonSetup()
+    updates |> Enum.reduce(0, fn update, acc -> acc + addUpValidMiddleNumbers(update, orders) end)
+  end
+
+  @doc """
+  Part 2
+  - Find the incorrect entries
+  - correct the order on them
+  - then add up the middle numbers.
+      iex>{:ok, contents} = File.read("sample.txt")
+      iex>Day5.part2(contents)
+      123
+  """
+  def part2(contents) do
+    [orders, updates] = contents |> commonSetup()
+    updates |> Enum.reduce(0, fn update, acc -> acc + addupCorrectedMiddleNumbers(update, orders) end)
   end
 
   @doc """
@@ -118,6 +197,17 @@ defmodule Day5 do
   def solve1() do
     {:ok, contents} = File.read("day5.txt")
     contents |> part1()
+  end
+
+   @doc """
+  Solve 2
+
+      iex> Day5.solve2()
+      4407
+  """
+  def solve2() do
+    {:ok, contents} = File.read("day5.txt")
+    contents |> part2()
   end
 
 end
